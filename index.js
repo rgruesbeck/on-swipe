ad66a94d9f7f5de683b6d6af7405686562796012
Ron Gruesbeck <rongruesbeck@gmail.com>
2019-07-31 16:20:47 -0700

    initial sketch

---
+++ index.js
@@ -0,0 +1,90 @@
+// onswipe
+
+// list touches
+var touches = [];
+
+// take touch list return a diffs for x and y
+const touchListDiffs = (touchList) => {
+    return touchList
+    .map((touch, idx, arr) => {
+        // collect diffs
+        let prev = arr[idx - 1] || arr[0];
+        return {
+            x: touch.x,
+            y: touch.y,
+            dx: touch.x - prev.x,
+            dy: touch.y - prev.y
+        }
+    })
+    .reduce((sum, diff) => {
+        // sum the diffs
+        sum.dx += diff.dx;
+        sum.dy += diff.dy;
+
+        return sum;
+    }, { dx: 0, dy: 0 });
+}
+
+// take diffs, return a swipe with a direction
+const diffSwipe = (diff) => {
+    return [diff]
+    .map(diff => {
+        return {
+            x: Math.abs(diff.dx) > Math.abs(diff.dy),
+            y: Math.abs(diff.dy) > Math.abs(diff.dx),
+            dx: diff.dx,
+            dy: diff.dy
+        };
+    })
+    .map(swipe => {
+        // get swipe direction
+        if (swipe.x) {
+            swipe.direction = swipe.dx > 0 ?
+            'right' : 'left';
+        }
+
+        if (swipe.y) {
+            swipe.direction = swipe.dy > 0 ?
+            'down' : 'up';
+        }
+
+        return {
+            dx: swipe.dx,
+            dy: swipe.dy,
+            direction: swipe.direction
+        };
+    })
+    .reduce(s => s);
+}
+
+const onSwipe = (type, touch, length, fn) => {
+    // reject non touch types
+    if (!type.match(/touchstart|touchmove|touchend/)) {
+        return;
+    }
+
+    // clear touch list
+    if (type === 'touchstart') {
+        touches = [];
+    }
+
+    // add to touch list
+    if (type === 'touchmove') {
+        let { clientX, clientY } = touch;
+        touches.push({ x: clientX, y: clientY });
+    }
+
+    // get user intention
+    if (type === 'touchend' && touches.length > length) {
+
+        // convert: touches -> diffs -> swipe
+        const swipe = [touches]
+        .map(touches => touchListDiffs(touches))
+        .map(diff => diffSwipe(diff))
+        .reduce(s => s);
+
+        fn(swipe);
+    }
+}
+
+export default onSwipe;
