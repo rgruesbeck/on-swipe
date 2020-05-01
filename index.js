8c820406e86275da633405155c91830b5d7a22c8
Ron Gruesbeck <rongruesbeck@gmail.com>
2020-05-01 10:09:31 -0400

    move to detail variable

--- index.js
+++ index.js
@@ -7,74 +7,74 @@ let touches = [];
 let eventOps = {};
 
 // distance
-const distance = (a, b) =>  {
+const distance = (a, b) => {
     return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 }
 
 // velocity
-const velocity = (d, t) =>  {
+const velocity = (d, t) => {
     return d / t;
 }
 
 // take touch list return a diffs for x and y
 const touchDiffs = (touchList) => {
     return touchList
-    .map((touch, idx, arr) => {
-        // collect diffs
-        let prev = arr[idx - 1] || arr[0];
-        return {
-            x: touch.x,
-            y: touch.y,
-            t: touch.t,
-            dx: touch.x - prev.x,
-            dy: touch.y - prev.y,
-            dt: touch.t - prev.t,
-        }
-    })
-    .reduce((sum, diff) => {
-        // sum diffs
-        sum.dx += diff.dx;
-        sum.dy += diff.dy;
-        sum.dt += diff.dt;
+        .map((touch, idx, arr) => {
+            // collect diffs
+            let prev = arr[idx - 1] || arr[0];
+            return {
+                x: touch.x,
+                y: touch.y,
+                t: touch.t,
+                dx: touch.x - prev.x,
+                dy: touch.y - prev.y,
+                dt: touch.t - prev.t,
+            }
+        })
+        .reduce((sum, diff) => {
+            // sum diffs
+            sum.dx += diff.dx;
+            sum.dy += diff.dy;
+            sum.dt += diff.dt;
 
-        return sum;
-    }, { dx: 0, dy: 0, dt: 0 });
+            return sum;
+        }, { dx: 0, dy: 0, dt: 0 });
 }
 
 // take diffs, return a swipe with a direction
 const parseDiff = (diff) => {
     return [diff]
-    .map(diff => {
-        return {
-            x: Math.abs(diff.dx) > Math.abs(diff.dy),
-            y: Math.abs(diff.dy) > Math.abs(diff.dx),
-            dx: diff.dx,
-            dy: diff.dy,
-            dt: diff.dt
-        };
-    })
-    .map(swipe => {
-        // get swipe direction
-        if (swipe.x) {
-            swipe.direction = swipe.dx > 0 ?
-            'right' : 'left';
-        }
+        .map(diff => {
+            return {
+                x: Math.abs(diff.dx) > Math.abs(diff.dy),
+                y: Math.abs(diff.dy) > Math.abs(diff.dx),
+                dx: diff.dx,
+                dy: diff.dy,
+                dt: diff.dt
+            };
+        })
+        .map(swipe => {
+            // get swipe direction
+            if (swipe.x) {
+                swipe.direction = swipe.dx > 0 ?
+                    'right' : 'left';
+            }
 
-        if (swipe.y) {
-            swipe.direction = swipe.dy > 0 ?
-            'down' : 'up';
-        }
+            if (swipe.y) {
+                swipe.direction = swipe.dy > 0 ?
+                    'down' : 'up';
+            }
 
-        return {
-            dx: swipe.dx,
-            dy: swipe.dy,
-            velocity: velocity(distance(swipe.dx, swipe.dy), swipe.dt),
-            distance: distance(swipe.dx, swipe.dy),
-            duration: swipe.dt,
-            direction: swipe.direction
-        };
-    })
-    .reduce(s => s);
+            return {
+                dx: swipe.dx,
+                dy: swipe.dy,
+                velocity: velocity(distance(swipe.dx, swipe.dy), swipe.dt),
+                distance: distance(swipe.dx, swipe.dy),
+                duration: swipe.dt,
+                direction: swipe.direction
+            };
+        })
+        .reduce(s => s);
 }
 
 const getSwipe = (type, touch, length, fn) => {
@@ -103,9 +103,9 @@ const getSwipe = (type, touch, length, fn) => {
 
         // convert: touches -> diffs -> swipe
         const swipe = [touches]
-        .map(touches => touchDiffs(touches))
-        .map(diff => parseDiff(diff))
-        .reduce(s => s);
+            .map(touches => touchDiffs(touches))
+            .map(diff => parseDiff(diff))
+            .reduce(s => s);
 
         fn(swipe);
     }
@@ -113,11 +113,12 @@ const getSwipe = (type, touch, length, fn) => {
 
 // broadcast swipe event
 const broadcastSwipe = (type, touch) => {
-
+    // get swipe and boardcast
     getSwipe(type, touch, moves, (swipe) => {
+        let detail = { detail: swipe };
 
         beacon.dispatchEvent(new CustomEvent('swipe', {
-            ...{ detail: swipe },
+            ...detail,
             ...eventOps
         }))
     });
@@ -139,6 +140,7 @@ const onSwipe = (ops) => {
     beacon.addEventListener('touchstart', ({ touches }) => broadcastSwipe('touchstart', touches[0]));
     beacon.addEventListener('touchmove', ({ touches }) => broadcastSwipe('touchmove', touches[0]));
     beacon.addEventListener('touchend', ({ touches }) => broadcastSwipe('touchend', touches[0]));
+
 }
 
 export default onSwipe;
